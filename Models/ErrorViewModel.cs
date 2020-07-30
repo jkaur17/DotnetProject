0e51f554c3ea069436696bfce99fe421be07a15a
Jaskirat Kaur <Jaskirat_Kaur@ad.infosys.com>
2020-07-30 16:51:45 +0530

    Added project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JasProj30.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
